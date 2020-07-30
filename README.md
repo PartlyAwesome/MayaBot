d693f4fe77e3f93534df85e369fcb14971bd9ba4
PartlyAwesome <5777950+PartlyAwesome@users.noreply.github.com>
2020-07-30 07:42:13 +0100

    it lives!
    pongs with ~ping
    
    accepts bot token as argument 1
    
    updated README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Mara Bot
 
 A Plural Proxy Bot *in Rust*
+
+Use it with `mara-bot [token]`
+
+Build it with `cargo`
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+extern crate exitcode;
+
+use serenity::client::Client;
+use serenity::model::channel::Message;
+use serenity::prelude::{EventHandler, Context};
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+use std::env;
+use std::process;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+fn main() {
+    // Login with a bot token from the environment
+    let args: Vec<_> = env::args().collect();
+    if args.len() < 2 {
+        println!("No Token provided as argument 1");
+        process::exit(exitcode::DATAERR);
+    }
+    let token = &args[1];
+    let mut client = Client::new(token, Handler)
+        .expect("Error creating client");
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+fn ping(ctx: &mut Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!")?;
+
+    Ok(())
+}
